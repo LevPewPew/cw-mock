3269e263b925ad1152636cc829a0eaaaea9bb3f3
Levente Koppany <levente.koppany@anz.com>
2022-04-20 12:56:47 +1000

    dt

--- navbar-app/webpack.config.js
+++ navbar-app/webpack.config.js
@@ -4,7 +4,7 @@ const singleSpaDefaults = require("webpack-config-single-spa-react");
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
     orgName: "acme",
-    projectName: "navbar-app-blahblahmuah",
+    projectName: "navbar-app",
     webpackConfigEnv,
     argv,
   });
