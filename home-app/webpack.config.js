e1a54637292ad305ca66a236aaa70ffd9f64063e
Levente Koppany <levente.koppany@anz.com>
2022-04-20 12:26:34 +1000

    dt

--- home-app/webpack.config.js
+++ home-app/webpack.config.js
@@ -4,7 +4,7 @@ const singleSpaDefaults = require("webpack-config-single-spa-react");
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
     orgName: "acme",
-    projectName: "home-app",
+    projectName: "home-app-foofizz",
     webpackConfigEnv,
     argv,
   });
