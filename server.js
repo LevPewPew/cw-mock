272af055401bd0e5a85595820bf40c3c76be6042
Levente Koppany <levente.koppany@anz.com>
2022-04-22 16:45:21 +1000

    heroku fiddling

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const path = require("path");
 const PORT = process.env.PORT || 5522;
 
 express()
-  .use(express.static(path.join(__dirname, "dist")))
+  .use(express.static(path.join(__dirname, "mfe-root/dist")))
   .get("*", (req, res) => {
     res.sendFile("index.html", { root: "mfe-root/dist" });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-// NEXT heroku should use node 14 not 16
+/* LEFTOFF when i deploy server to heroku, it is looking in mfe-root/dist, but
+  ofc that doesn't exist as it hasn't been built on the server
+  
+  either i need to build it within heroku somehow... or look at the tute i have 
+  been following i am sure it explains what i should be doing
+
+  or should the server be pulling it from s3, not the heroku server/env?
+  */
