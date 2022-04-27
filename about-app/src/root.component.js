e55ba63bac1205315b10cd88a28ee549bac77520
Levente Koppany <levente.koppany@anz.com>
2022-04-27 11:12:26 +1000

    a change on about page

--- about-app/src/root.component.js
+++ about-app/src/root.component.js
@@ -14,7 +14,7 @@ export default function Root(props) {
       <p className="lead">
         Hey, what's up?
         <br />
-        This is a page to talk about us.
+        This is a page to talk about us. A CHANGE ON ABOUT PAGE.
       </p>
     </section>
   );
