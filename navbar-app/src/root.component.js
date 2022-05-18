08cac8116dbb0b362326da97e7f747ffc6c3cc82
Levente Koppany <levente.koppany@anz.com>
2022-05-18 16:18:23 +1000

    using reach router

--- navbar-app/src/root.component.js
+++ navbar-app/src/root.component.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { Link } from "@reach/router";
+
+function NavLink(props) {
+  return (
+    <Link
+      {...props}
+      className="nav-link"
+      getProps={({ isCurrent }) => {
+        return {
+          className: `nav-link ${isCurrent ? "active" : false} `,
+        };
+      }}
+    />
+  );
+}
 
 export default function Root(props) {
   return (
-    <header className="masthead mb-auto ">
+    <header className="mb-auto masthead ">
       <div className="inner">
         <a className="blog-logo" href="/">
           <img
@@ -10,18 +25,12 @@ export default function Root(props) {
             src="https://single-spa.js.org/img/logo-white-bgblue.svg"
             alt="Single Spa logo"
           />{" "}
-          React + single-spa
+          Reload Site and Root App
         </a>
-        <nav className="nav nav-pills p-4">
-          <a className={`nav-link ${location.pathname === "/" && "active"}`} href="/">
-            Home
-          </a>
-          <a className={`nav-link ${location.pathname === "/about" && "active"}`} href="/about">
-            About Us
-          </a>
-          <a className={`nav-link ${location.pathname === "/contact" && "active"}`} href="/contact">
-            Contact
-          </a>
+        <nav className="p-4 nav nav-pills">
+          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/about"}>About Us</NavLink>
+          <NavLink to={"/contact"}>Contact</NavLink>
         </nav>
       </div>
     </header>
