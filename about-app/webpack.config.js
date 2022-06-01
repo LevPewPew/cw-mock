a3fa92929bbbd6476c1e874566dfeeb8f04f146d
Levente Koppany <levente.koppany@anz.com>
2022-06-01 17:22:18 +1000

    getting extra in-browser imports working

--- about-app/webpack.config.js
+++ about-app/webpack.config.js
@@ -11,8 +11,8 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-    // externals: {
-    //   lodash: '_',
-    // },
+    externals: {
+      lodash: "lodash",
+    },
   });
 };
