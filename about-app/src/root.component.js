5db293eefb258d5c478d3a77e8688d73773785f3
Levente Koppany <levente.koppany@anz.com>
2022-06-07 11:02:44 +1000

    wrapping up

--- about-app/src/root.component.js
+++ about-app/src/root.component.js
@@ -1,11 +1,8 @@
 import React, { useEffect } from "react";
 import Parrot from "./shared-ui/components/parrot";
 import GarlicBread from "./shared-ui/components/garlic-bread";
-import BasicChart from "./shared-ui/components/basic-chart";
 import appStore from "./shared-ui/stores/app-store";
-// import { Provider } from "mobx-react";
 import { Provider } from "mobx-react";
-import lowdish from "lodash";
 
 // dummy function to simulate fetching data from server
 async function getCookingStatus() {
@@ -36,11 +33,10 @@ export default function Root() {
         <div className="homepage-hero" style={{ margin: "5rem 0" }}>
           <img src="https://www.stevensegallery.com/600/400" alt="aerial shot of land and sea" />
         </div>
-        <h1 className="cover-heading">{lowdish.upperCase("about-us")}</h1>
+        <h1 className="cover-heading">About Us</h1>
         <p className="lead">This is a page to talk about us. We are amazing. Send us money.</p>
         <Parrot />
         <GarlicBread />
-        <BasicChart />
       </section>
     </Provider>
   );
--- about-app/webpack.config.js
+++ about-app/webpack.config.js
@@ -11,8 +11,5 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-    externals: {
-      lodash: "lodash",
-    },
   });
 };
