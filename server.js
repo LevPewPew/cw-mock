3bebd470f53ff4baef481fa161331ecc0e33c2b6
Levente Koppany <levente.koppany@anz.com>
2022-07-07 08:54:46 +1000

    removed doubled up root config for deployed and served app

--- server.js
+++ server.js
@@ -8,9 +8,3 @@ express()
     res.sendFile("index.html", { root: "dist" });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-/* NEXT seems like doubling up on the root. it is built in heroku and served from there,
-but it is also built in S3 and the index built in heroku is actually pulling it from
-the bucket via the import map. i think?
-
-it doesn't create any problems but it's not a "clean" solution */
